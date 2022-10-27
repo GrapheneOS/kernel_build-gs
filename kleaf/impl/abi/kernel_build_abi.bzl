reocat/android_kernel_build

kleaf: extract_symbol with GKI modules objects
To partition between GKI and vendor modules on the fly;
we need to pass GKI modules from the base_kernel build
in addition to vendor modules. Remove passing the list
and pass the .ko files instead from  the base_kernel.

Bug: 232431411
Test: TH
Signed-off-by: Ramji Jiyani <ramjiyani@google.com>
Change-Id: I2f40943d0d4f0a65cc7ab47a5064caff7fbf39b4

kleaf/impl/abi/kernel_build_abi.bzl
@@ -284,6 +284,7 @@ def _define_abi_targets(
         src = kernel_build_kwargs.get("kmi_symbol_list"),
         kmi_symbol_list_add_only = kmi_symbol_list_add_only,
         tags = tags,
+        gki_modules_src = kernel_build_kwargs.get("base_kernel", name),
     )
     update_source_file(
         name = name + "_abi_update_symbol_list",

GrapheneOS/kernel_build-gs

Due to change f67eea6aa43b30326960b39fe5f4c99c1519b86a
(kleaf: Do not include GKI modules in default output of
//common:kernel_aarch64.) and relevant changes, GKI modules
are no longer listed as default outputs of the GKI kernel
//common:kernel_aarch64, causing the `in_tree_modules` list
to be empty in extracted_symbols rule. Backporting this change
fixes the issue by also extracting the full modules_staging_dir,
which contains all GKI modules.
Bug: 246716067
Test: manual (bazel run //common:kernel_aarch64_abi_update_symbol_list,
  see no change)
Signed-off-by: Yifan Hong <elsk@google.com>
----
Original commit message:
and pass the modules instead from the base_kernel
first and then the vendor modules overwriting the modules
from the base build being overridden.
Bug: 245404948

kleaf/impl/abi/kernel_build_abi.bzl
@@ -280,6 +280,7 @@ def _define_abi_targets(
         # modules list from base_kernel (GKI). If base_kernel is not set, this
         # likely a GKI build, so use modules_outs from itself.
         gki_modules_list_kernel_build = kernel_build_kwargs.get("base_kernel", name),
+        kernel_build_for_base_modules = kernel_build_kwargs.get("base_kernel", name),
     )
     update_source_file(
         name = name + "_abi_update_symbol_list",